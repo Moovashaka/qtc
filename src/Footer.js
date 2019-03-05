5f26bb458e253b0790eba716fbf7cedddf918872
Moova Shaka <mrmoovashaka@gmail.com>
2019-03-05 19:29:30 +0000

    adding link

--- src/Footer.js
+++ src/Footer.js
@@ -9,6 +9,7 @@ class Footer extends Component {
       <p>Tel: 07710 506269</p>
       <p>Email: info@qtc-ltd.co.uk</p>
       <p>VAT Reg No: 828 2280 26 &nbsp; &nbsp; Company Reg No: 4884683</p>
+      <p><a href="https://www.moovashaka.co.uk" noopener noreferer target="blank">Powered by MoovaShaka</a></p>
       </div>
     );
   }
