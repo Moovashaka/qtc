c32e5c6875adeccbc5ee4820d65a62255668810d
Moova Shaka <mrmoovashaka@gmail.com>
2019-03-05 11:55:29 +0000

    qtc first commit

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+      <p>Registered Office: Henry Morgan House, Industry Road, Carlton, Barnsley S71 3PQ </p>
+      <p>Tel: 07710 506269</p>
+      <p>Email: info@qtc-ltd.co.uk</p>
+      <p>VAT Reg No: 828 2280 26 &nbsp; &nbsp; Company Reg No: 4884683</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
