352349b58a23907bd037a76add0e931fef3de8df
Moova Shaka <mrmoovashaka@gmail.com>
2020-01-31 17:31:51 +0000

    updating address

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,8 @@ class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-      <p>Regd. Office: QTC Ltd, Barleycroft House, 5a Barleycroft Lane, Dinnington</p>
-      <p>Sheffield S25 2LE</p>
+      <p>Regd. Office: QTC Ltd, Lifestyle House, 157-167 Sheffield Road</p>
+      <p>Sheffield S21 1DY</p>
       <p>Tel: 07710 506269</p>
       <p>Email: info@qtc-ltd.co.uk</p>
       <p>VAT Reg No: 828 2280 26 &nbsp; &nbsp; Company Reg No: 4884683</p>
